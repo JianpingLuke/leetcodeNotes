03f898d0780505250b360d9b1440ae8fcf3785ad
xiaodyao <31445919+xiaodyao@users.noreply.github.com>
2021-06-07 15:28:47 -0400

    92

--- List/src/ReverseTwo_92.java
+++ List/src/ReverseTwo_92.java
@@ -7,16 +7,27 @@ public class ReverseTwo_92 {
                 cur = cur.next;
                 count++;
             }
-            ListNode prev = cur;
-            cur = cur.next;
-            ListNode remain = cur.next;
-            while (count < right && remain != null) {
-                prev.next = remain;
-                cur.next = remain.next;
-                remain.next = cur;
-                remain = cur.next;
+            ListNode remain = null;
+            if (count != left){ // 并不一定有remain的存在，如果没有，则是第一个开始就转，此时单独讨论。
+                remain = cur;
+                cur = cur.next;
                 count++;
             }
+            ListNode prev = cur, tail = cur;
+            if (cur == null) return head;
+            cur = cur.next;
+            count++;
+            while (count <= right  && cur != null) { // 注意换位的规范
+                prev.next = cur.next;
+                cur.next = tail;
+                tail = cur;
+                cur = prev.next;
+                count++;
+            }
+            if (remain == null){
+                return tail;
+            }
+            remain.next = tail;
             return head;
         }
     }
